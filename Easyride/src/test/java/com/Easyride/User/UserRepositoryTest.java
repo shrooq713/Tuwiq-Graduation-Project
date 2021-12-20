8b26b383ad444e81c6e1811142558afe7198cddd
Shrooq713 <shroog713@gmail.com>
2021-12-20 14:16:06 +0300

    setup

---
+++ Easyride/src/test/java/com/Easyride/User/UserRepositoryTest.java
@@ -0,0 +1,13 @@
+package com.Easyride.User;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Test
+    void findByUserName() {
+    }
+}
