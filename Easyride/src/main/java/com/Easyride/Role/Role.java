493aec9720f567140b0bdb1e1ebe895dc7498504
Shrooq713 <shroog713@gmail.com>
2021-12-20 10:47:16 +0300

    create-role

---
+++ Easyride/src/main/java/com/Easyride/Role/Role.java
@@ -0,0 +1,15 @@
+package com.Easyride.Role;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+}
